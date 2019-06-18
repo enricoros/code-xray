cd81a6109dcc977de941be8e5c6cd8e87ef61de1
Enrico Ros <enrico.ros@gmail.com>
2019-06-17 21:34:01 -0700

    App: improve looks, use simplified Sign-in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,87 @@
 import React from 'react';
-import './App.css';
 import Button from '@material-ui/core/Button';
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import {makeStyles} from '@material-ui/core/styles';
+import Link from "@material-ui/core/Link";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+// import createPersistedState from 'use-persisted-state';
+import './App.css';
+import SignIn from "./SignIn";
 
+// localstorage persisted state
+// const usePersistedUserState = createPersistedState('user_name_2');
+
+// settings
+const default_GuestName = 'Guest';
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+  heroContent: {
+    padding: theme.spacing(8, 0, 6),
+  },
+}));
+
+function SectionHeader(props) {
+  return <Container maxWidth="md" component="main" className={props.heroClass}>
+    <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+      {props.title}
+    </Typography>
+    <Typography variant="h5" align="center" color="textSecondary" component="p">
+      {props.description}
+    </Typography>
+  </Container>;
+}
 
 function App() {
+  const classes = useStyles();
+
+  // ask for user name if not set
+  const [userName, setUserName] = React.useState(default_GuestName);
+  if (!userName) return <SignIn onUserChanged={setUserName}/>;
+
   return (
-    <div className="App">
-      <Typography variant="h1">
-        h1. test
-      </Typography>
-      <Typography variant="h2">
-        h2. test
-      </Typography>
-      <Typography variant="h3">
-        h3. test
-      </Typography>
-      <h1>test</h1>
-      <h2>test</h2>
-      <h3>test</h3>
-      <header className="App-header">
-        Hi. <Button variant="contained" color="primary">Hello World</Button>
-      </header>
-    </div>
+    <React.Fragment>
+      {/* Navigation */}
+      <AppBar position="static" color="default" className={classes.appBar}>
+        <Container maxWidth="lg">
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h4" color="inherit" noWrap className={classes.toolbarTitle}>Code XRay</Typography>
+            <nav>
+              <Link variant="button" color="textPrimary" href="#" className={classes.link}
+                    component="a">{userName}</Link>
+            </nav>
+            <Button href="#" color="primary" variant="outlined" className={classes.link}
+                    onClick={() => setUserName(undefined)}>Logout</Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {/* Welcome */}
+      <SectionHeader heroClass={classes.heroContent} title="Code XRay"
+                     description="Quickly understand a project based on analysis of the source code"/>
+      {/* Section 1: Load file */}
+      <Container maxWidth="lg">
+        <Typography variant="h4" component="h1" gutterBottom>
+          Welcome {userName}
+        </Typography>
+        <header className="App-header">
+          Hi. <Button variant="contained" color="primary">Hello World</Button>
+        </header>
+      </Container>
+
+    </React.Fragment>
   );
 }
 
