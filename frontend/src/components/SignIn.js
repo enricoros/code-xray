3530f1427498d5bf4737254d33e38206877b2b9a
Enrico Ros <enrico.ros@gmail.com>
2019-06-25 01:36:19 -0700

    Fix SignIn

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar/index';
-import Button from '@material-ui/core/Button/index';
-import CssBaseline from '@material-ui/core/CssBaseline/index';
-import TextField from '@material-ui/core/TextField/index';
+import {makeStyles} from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography/index';
-import {makeStyles} from '@material-ui/core/styles/index';
-import Container from '@material-ui/core/Container/index';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -43,7 +42,6 @@ export default function SignIn(props) {
   const [userName, setUserName] = React.useState(initialName || '');
   return (
     <Container component="main" maxWidth="xs">
-      <CssBaseline/>
       <div className={classes.paper}>
         <Avatar className={classes.avatar} component="div"><LockOutlinedIcon/></Avatar>
         <Typography component="h1" variant="h5">What's your name?</Typography>
