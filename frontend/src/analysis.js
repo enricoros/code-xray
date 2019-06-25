6b9da5c091f7c872d3ac129178db6a2051e0ada9
Enrico Ros <enrico.ros@gmail.com>
2019-06-24 20:05:32 -0700

    Improve analysis of language statistics massively
    Using lots of [].reduce over uniform formats.

--- frontend/src/analysis.js
+++ frontend/src/analysis.js
@@ -2,57 +2,57 @@ import path from "path";
 import {DEBUGGING} from "./config";
 
 
-// constants
-const DEFAULT_NO_LANGUAGES = [
-  'XML', 'YAML', 'Dockerfile', 'Protocol Buffers', 'HTML', 'Bourne Shell', 'Markdown', 'CMake',
-  'PowerShell', 'Windows Module Definition', 'DOS Batch', 'Pascal', 'MSBuild script'
-];
-
-
 // parse CLOC JSON to a list of files: {name, dir, code, language}
-function clocJsonToFilesStats(cj) {
+export function clocJsonToFilesStats(cj) {
   if (DEBUGGING) {
     console.log('Loaded a file generated from cloc: ' + cj['header']['cloc_version']);
     console.log(cj['header']);
-    console.log(cj['SUM']);
   }
   delete cj['header'];
   delete cj['SUM'];
-  const list = [];
+  const l = [];
   for (const [filePath, clocFile] of Object.entries(cj)) {
     if (!filePath.startsWith('./'))
       throw new Error('Expected all files to begin with "./", this is wrong: ' + filePath);
-    list.push({
+    l.push({
       'name': path.basename(filePath.substring(2)),
       'dir': path.dirname(filePath.substring(2)),
-      'code': clocFile['code'],
-      'blank': clocFile['blank'],
-      'comment': clocFile['comment'],
-      'language': clocFile['language'],
+      'langStats': {
+        'name': clocFile['language'],
+        'code': clocFile['code'],
+        'blank': clocFile['blank'],
+        'comment': clocFile['comment'],
+        'files': 1.0,
+      },
       // '_': clocFile,
     });
   }
-  return list;
+  return l;
 }
+
+const sortAscByKpi = kpi => (a, b) => a[kpi] - b[kpi];
+export const sortDescByKpi = kpi => (a, b) => b[kpi] - a[kpi];
 
 // language statistics for the project
-function langStatsFromFilesStats(filesStats) {
-  let languages = {};
-  filesStats.forEach((f) => languages[f.language] = (languages[f.language] || 0) + f.code);
-  languages = Object.entries(languages)
-    .map((entry) => {
-      return {
-        'name': entry[0],
-        'code': entry[1],
-      }
-    })
-    .sort((a, b) => b['code'] - a['code']);
-  // const total = languages.reduce((prev, curr) => prev + curr['code'], 0);
-  return languages;
+export function accumulateLangStats(langsStats) {
+  return langsStats.reduce((acc, src) => {
+    let lang = acc.find(l => l.name === src.name);
+    if (!lang)
+      acc.push(lang = {name: src.name});
+    ['code', 'blank', 'comment', 'files'].forEach(
+      kpi => lang[kpi] = (lang[kpi] || 0) + src[kpi]);
+    return acc;
+  }, []);
 }
 
-//
+export function langStatsFromFilesStats(filesStats) {
+  return accumulateLangStats(filesStats.map(f => f.langStats))
+    .sort(sortDescByKpi('code'));
+}
 
-
-export {clocJsonToFilesStats, langStatsFromFilesStats, DEFAULT_NO_LANGUAGES}
-
+export function langsSumStats(langsStats) {
+  return langsStats.reduce((acc, langStats) => {
+    ['code', 'blank', 'comment', 'files'].forEach(kpi => acc[kpi] = (acc[kpi] || 0) + langStats[kpi]);
+    return acc;
+  }, {name: '_SUM_'});
+}
